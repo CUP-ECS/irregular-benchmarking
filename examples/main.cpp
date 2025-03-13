be9b2e21cde40dd921f6a4de5da04526f6d75f28
nicholas <nbacon@unm.edu>
2025-03-13 02:28:30 -0600

    chaseing down odd bug

--- examples/main.cpp
+++ examples/main.cpp
@@ -221,7 +221,9 @@ void migrationExample() {
         //for (int it = 0; it < niterations; ++it) {
             Kokkos::View < int * , MemorySpace > export_ranks("export_ranks", num_tuple);
             int num_indices_offpe = 0;
-
+        for (int i = 0; i < num_tuple; ++i) {
+            export_ranks(i) = -1;
+        }
 
             for (int i = 0; i < nneighbors; i++) {
                 int inum = 0;
