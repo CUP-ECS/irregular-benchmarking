c07de4cfbd61bfaab81720a931704a86c1b6f74d
nicholas <nbacon@unm.edu>
2025-04-16 22:28:17 -0600

    error fixed

--- examples/main.cpp
+++ examples/main.cpp
@@ -388,6 +388,7 @@ void run_benchmark()
     	for (double t : time) {
         	oss << t << " ";
     	}
+
         std::string result = oss.str();
         printf("nowned - %d, nremote - %d,  blocksize - %d,stride - %d,nneighbors - %d %s\n", nowned,nremote,blocksz,stride,nneighbors,result.c_str());
 	}
