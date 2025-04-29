726bb38b4838c2cc01514a01725a9ca0fb7dbb25
nicholas <nbacon@unm.edu>
2025-04-29 10:45:04 -0600

    chasing bugs 546545sdfg4

--- examples/main.cpp
+++ examples/main.cpp
@@ -411,11 +411,12 @@ void run_benchmark()
 				Cabana::Distributor<MemorySpace> distributor(MPI_COMM_WORLD, export_ranks);
 
 			if(50 == comm_rank){
-                   size_t n = std::min(distributor.numNeighbor(), neighbors.size());
+                                    size_t n = std::min(distributor.numNeighbor(), neighbors.size());
 for (size_t i = 0; i < n; ++i) {
-    std::cout << "(" << distributor_neighbors[i] << ", " << neighbors[i] << ") ";
+    std::cout << "(" << distributor.neighborRank(i) << ", " << neighbors[i] << ") ";
 }
 std::cout << std::endl;
+
 			}
 
 
