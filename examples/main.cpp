97de51bb8f50350c194effcf28aa96b58e5bc278
Nicholas <nbacon@unm.edu>
2025-06-23 14:16:50 -0600

    debugging test

--- examples/main.cpp
+++ examples/main.cpp
@@ -427,6 +427,9 @@ void parse_config_file(std::string config_file)
 					bins.push_back(bin);
 			}
 
+			std::sort(bins.begin(), bins.end(), [](const Bin& a, const Bin& b) {
+				return a.bin_prop < b.bin_prop;  // Sorts in ascending order
+			});
 
             if (name == "comm_partners")
 			{
