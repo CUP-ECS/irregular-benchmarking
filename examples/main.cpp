e21cef0b7cb8bbfa4680007ef1a800dd0cb45fb7
Nicholas <nbacon@unm.edu>
2025-08-08 13:30:48 -0600

    error checking

--- examples/main.cpp
+++ examples/main.cpp
@@ -748,7 +748,7 @@ int main(int argc, char **argv)
 
 
 		Kokkos::ScopeGuard scope_guard(argc, argv);
-
+		mpix_neighbor_alltoallv_init_implementation = NEIGHBOR_ALLTOALLV_INIT_STANDARD;
         // Run the benchmark
 		run_benchmark();
 	}
